240cc3d8f5a3fed5abdf0bb76c0bceec6670c962
Juliana <Juliana@LAPTOP-D1IJKFMU>
2023-09-18 11:45:46 -0500

    Updated

---
+++ src/pooere10/POOERE10.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pooere10;
+
+/**
+ *
+ * @author Juliana
+ */
+public class POOERE10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+    
+}
